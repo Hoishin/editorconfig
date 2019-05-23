d586785f7d726888c36ed78620ae1d4607d932cc
Hoishin <hoishinxii@gmail.com>
2019-05-23 23:29:39 +0900

    notify updates

--- src/bin.ts
+++ src/bin.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
+import updateNotifier from 'update-notifier';
 
-const readStream = fs.createReadStream(path.resolve(__dirname, "editorconfig"));
-const writeStream = fs.createWriteStream(".editorconfig");
+updateNotifier({pkg: require('../package.json')}).notify();
+
+const readStream = fs.createReadStream(path.resolve(__dirname, 'editorconfig'));
+const writeStream = fs.createWriteStream('.editorconfig');
 
 readStream.pipe(writeStream);
