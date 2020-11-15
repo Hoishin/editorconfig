775ca6a174b4b3d6af8b375dc49227d5d7d189cb
Keiichiro Amemiya <hoishinxii@gmail.com>
2020-11-15 07:50:46 -0800

    fix: dist directory structure

--- src/bin.ts
+++ src/bin.ts
@@ -3,11 +3,6 @@
 import fs from "fs";
 import path from "path";
 
-import updateNotifier from "update-notifier";
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-updateNotifier({pkg: require("../package.json")}).notify();
-
 const readStream = fs.createReadStream(path.resolve(__dirname, "editorconfig"));
 const writeStream = fs.createWriteStream(".editorconfig");
 
